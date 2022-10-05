8b93ddcb7fc8fafde7e88141e53eaeaaca135fbc
IvanZielinski <56046501+IvanZielinski@users.noreply.github.com>
2022-10-05 16:28:17 -0300

    Se hace publico Void main

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 
 namespace Ejercicio_EY
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Enter username:");
 
